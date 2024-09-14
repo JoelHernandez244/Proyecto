5395c25ca6492cadc5c18611de94864b708960df
Are23049 <arely1adma2020@gmail.com>
2024-09-13 21:34:04 -0600

    Update Form1.cs
    boton calcular

--- Form1.cs
+++ Form1.cs
@@ -22,9 +22,41 @@ namespace Proyecto
         {
             Close();
         }
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
 
-      
+            double sueldo = Double.Parse(txtSueldo.Text);
+            double renta;
+            txtRenta.Text = "";
+            if (sueldo <= 325)
+            {
+                renta = 0;
+            }
+            else if (sueldo <= 700)
+            {
+                renta = sueldo * 0.15;
+            }
+            else if (sueldo <= 1100)
+            {
+                renta = sueldo * 0.17;
+            }
+            else if (sueldo <= 2000)
+            {
+                renta = sueldo * 0.21;
+            }
+            else if (sueldo <= 2900)
+            {
+                renta = sueldo * 0.25;
+            }
+            else
+            {
+                renta = sueldo * 0.29;
+            }
+            txtRenta.Text = renta.ToString();
+            MessageBox.Show("Total a pagar Renta: " + renta, "Salida", MessageBoxButtons.OK);
             
         }
     }
 }
+
+        
